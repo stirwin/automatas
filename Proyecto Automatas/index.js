8030ee71f5d69759b5294251908778280296bdd2
edwin <edwintorres0115@hotmail.com>
2023-09-16 20:36:25 -0500

    If solucionado

--- Proyecto Automatas/index.js
+++ Proyecto Automatas/index.js
@@ -2,15 +2,18 @@ function declarar(){
     let texto = document.getElementById("texto").value;
     var regex = /^declare\s+([a-zA-Z_][a-zA-Z0-9_]{0,14}(,[a-zA-Z_][a-zA-Z0-9_]{0,14}){0,14})\s+(entero|real|cadena|lógico|fecha);$/;
     var resultado = regex.test(texto);
-    document.getElementById("personaje").style.display = 'none';
-    document.getElementById("tituloMensaje").style.display = 'none';
-    document.getElementById("parrafo").style.display = 'none'
-    document.getElementById("texto2").style.display = 'flex';
-    document.getElementById("texto2").value = resultado ? "Sentencia válida" : "Sentencia inválida";
-    if(texto.length ==0){
+ 
+    if(texto.length != 0){
+        document.getElementById("personaje").style.display = 'none';
+        document.getElementById("tituloMensaje").style.display = 'none';
+        document.getElementById("parrafo").style.display = 'none'
+        document.getElementById("texto2").style.display = 'flex';
+        document.getElementById("texto2").value = resultado ? "Sentencia válida" : "Sentencia inválida";
+    }
+    else{
         swal("oops","Debes ingresar un texto","warning");
     }
-}
+} 
 
 function limpiar(){
     let texto = document.getElementById("texto").value;
